0c1b0c55d9319a18eb81b2e5775b7799e28291a5
Jesse <jessefentress246@gmail.com>
2022-04-19 14:02:36 -0400

    make it better

--- aslAPP/mediapipe_hands.py
+++
@@ -1,8 +0,0 @@
-import mediapipe as mp
-
-class MediaPipeHands:
-
-    def __init__(self):
-        self.mp_drawing = mp.solutions.drawing_utils
-        self.mp_drawing_styles = mp.solutions.drawing_styles
-        self.mp_hands = mp.solutions.hands
