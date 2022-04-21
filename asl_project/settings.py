373f23f987d28b85b9760add6c91fef81adb6ffe
JesseFentress <jessefentress246@gmail.com>
2022-04-21 02:15:20 -0400

    d

--- asl_project/settings.py
+++ asl_project/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,7 +131,6 @@ STATIC_DIRS = [
     os.path.join(BASE_DIR, 'aslAPP/static')
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 # Default primary key field type
