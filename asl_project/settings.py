d5292b0a7b1850102a3b5315deac5ce14380823a
JesseFentress <jessefentress246@gmail.com>
2022-04-21 01:57:29 -0400

    adding whitenoise

--- asl_project/settings.py
+++ asl_project/settings.py
@@ -127,6 +127,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'aslAPP/static/')
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
